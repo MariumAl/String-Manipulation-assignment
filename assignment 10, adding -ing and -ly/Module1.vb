01b2cd0b32b10deea88c3a315b13422424f3e615
MariumAl <93540809+MariumAl@users.noreply.github.com>
2021-11-16 21:19:16 +0500

    Add files via upload

---
+++ assignment 10, adding -ing and -ly/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, lastChars As String
+
+        str1 = ""
+        lastChars = ""
+
+        Console.Write("Enter string to process: ")
+        str1 = Console.ReadLine
+        lastChars = Right(str1, 3)
+        If Len(str1) < 3 Then
+            Console.Write("Processed string: " & str1)
+        ElseIf lastChars = "ing" Then
+            Console.Write("Processed string : " & str1 & "ly")
+        Else : Console.Write("Processed string: " & str1 & "ing")
+
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
