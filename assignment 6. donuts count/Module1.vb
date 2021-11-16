93522343dbed7d779cbc0bc480234bce5d6b0658
MariumAl <93540809+MariumAl@users.noreply.github.com>
2021-11-16 21:11:10 +0500

    Add files via upload

---
+++ assignment 6. donuts count/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim count As Integer
+        count = 0
+        Console.Write("Input number of donuts:")
+        count = Console.ReadLine()
+        If count <= "9" And count >= "0" Then
+            Console.WriteLine("Number of Donuts: " & count)
+        ElseIf count >= "10" Then
+            Console.WriteLine("Number of donuts: Many.")
+        Else : Console.WriteLine("invalid input.")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
