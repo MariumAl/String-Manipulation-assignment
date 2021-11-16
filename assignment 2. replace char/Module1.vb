5b3c2c60b103cd793e415144318385269a3b1f6c
MariumAl <93540809+MariumAl@users.noreply.github.com>
2021-11-16 21:01:00 +0500

    Add files via upload

---
+++ assignment 2. replace char/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim str1, str2 As String
+        Dim char1, char2, thischar As Char
+        Dim count As Integer
+        count = 0
+        char1 = ""
+        str1 = ""
+        char2 = ""
+        thischar = ""
+        str2 = ""
+
+        Console.WriteLine("Enter string to process: ")
+        str1 = Console.ReadLine
+        Console.WriteLine("Enter charcter to replace: ")
+        char1 = Console.ReadLine
+        Console.WriteLine("Enter character to replace with: ")
+        char2 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            thischar = Mid(str1, count, 1)
+            If thischar = char1 Then
+                thischar = char2
+
+            End If
+            str2 = str2 & thischar
+
+        Next
+
+        Console.WriteLine("processed string: " & str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
+
+
+
