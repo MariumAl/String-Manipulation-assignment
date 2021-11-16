1efd1ded46713d24b54f58b963d763fb4659c0f0
MariumAl <93540809+MariumAl@users.noreply.github.com>
2021-11-16 21:15:41 +0500

    Add files via upload

---
+++ assignment 8. replacing the character same as first except first/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2 As String
+        Dim thisChar, nextChar As Char
+        Dim x As Integer
+        str2 = ""
+        x = 0
+        thisChar = ""
+        nextChar = ""
+        str1 = ""
+        Console.Write("Iput string to process: ")
+        str1 = Console.ReadLine()
+            thisChar = Mid(str1, 1, 1)
+        For x = 2 To Len(str1)
+            nextChar = Mid(str1, x, 1)
+            If nextChar = thisChar Then
+                nextChar = "*"
+            Else : nextChar = Mid(str1, x, 1)
+            End If
+            str2 = str2 & nextChar
+
+        Next
+        str2 = thisChar & str2
+        Console.WriteLine("proccessed string: " & str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
